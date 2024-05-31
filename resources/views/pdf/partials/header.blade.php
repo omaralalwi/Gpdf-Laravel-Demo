8b52e92dfdb51115221c7372a6c78f91f81d2caa
omaralalwi <o.alalwi@iajeer.com>
2024-06-01 01:46:14 +0300

    fix

--- resources/views/pdf/partials/header.blade.php
+++ resources/views/pdf/partials/header.blade.php
@@ -1,4 +1,21 @@
+@php
+    $path = public_path('images/company-logo.png');
+    $type = pathinfo($path, PATHINFO_EXTENSION);
+    $data = file_get_contents($path);
+    $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+@endphp
+
 <header>
-    <h2>Fixed Header</h2>
-    <p>Additional header content here</p>
+    <div style="display: flex; align-items: center; justify-content: space-between;">
+        <div style="flex: 1;">
+            <img src="{{ $base64 }}" alt="Company Logo" style="height: 50px;">
+        </div>
+        <div style="flex: 2; text-align: center;">
+            <h2>Gpdf Team</h2>
+            <p>Invoice Title</p>
+        </div>
+        <div style="flex: 1; text-align: right;">
+            <p>Date: {{ \Carbon\Carbon::now()->format('Y-m-d') }}</p>
+        </div>
+    </div>
 </header>
