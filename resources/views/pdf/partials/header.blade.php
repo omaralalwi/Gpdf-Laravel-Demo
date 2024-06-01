cdb90da3e17aeecc5465b2773c9e90e48f260e9c
omaralalwi <o.alalwi@iajeer.com>
2024-06-01 03:20:47 +0300

    clean up

--- resources/views/pdf/partials/header.blade.php
+++ resources/views/pdf/partials/header.blade.php
@@ -6,20 +6,6 @@
 @endphp
 
 <header>
-{{--    <div style="display: flex; align-items: center; justify-content: space-between;">--}}
-{{--        <div style="flex: 1;">--}}
-{{--            <img src="{{ $base64 }}" alt="Company Logo" style="height: 50px;">--}}
-{{--        </div>--}}
-{{--        <div style="flex: 2; text-align: center;">--}}
-{{--            <p>Company Name</p>--}}
-{{--            <p>Invoice Title</p>--}}
-{{--        </div>--}}
-{{--        <div style="flex: 1; text-align: right;">--}}
-{{--            <p>Date: {{ \Carbon\Carbon::now()->format('Y-m-d') }}</p>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-
-
     <table class="table">
         <tbody>
         <tr>
